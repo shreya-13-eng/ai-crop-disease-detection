5bec154000aade07ae47b6437eabd66d733a8d6b
shreya-13-eng <shreyasharma79194@gmail.com>
2026-03-14 20:32:56 +0530

    Project's config v2

--- src/main/java/com/metaminds/aicropdiseasedetection/core/services/EmailService.java
+++ src/main/java/com/metaminds/aicropdiseasedetection/core/services/EmailService.java
@@ -1,11 +1,12 @@
 package com.metaminds.aicropdiseasedetection.core.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Async
 @Service
 public class EmailService {
     private final JavaMailSender mailSender;
